afcb4334442d46fc974e409b92c677a237d87a7e
user <user@localhost>
2023-10-12 10:52:47 +0700

    composables

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="index">
         <h1>Index page</h1>
+        <h2>{{ foo }}</h2>
         <Hello />
     </div> 
-  </template>
+</template>
+
+<script setup lang="ts">
+    const foo = useFoo();
+</script>
